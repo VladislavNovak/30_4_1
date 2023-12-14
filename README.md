4fbde9dae658b207de44e5236efa01936b01beee
Novak <novaldix@gmail.com>
2023-12-14 22:09:19 +0300

    30_4_1 added get and post requests

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <details open>
-<summary><span style="color:tomato;font-size:16px">HTTP_REQUESTS</span></summary>
+<summary><span style="color:tomato;font-size:16px">HTTP запросы</span></summary>
 <details open>
 <summary><span style="color:tomato;font-size:12px">Пользовательские запросы</span></summary>
 
@@ -11,6 +11,44 @@
 Содержимое ответа сервера выводится в стандартный вывод.
 
 Программа завершается, когда пользователь вводит команду “exit”.
+
+</details>
+
+<details open>
+<summary><span style="color:tomato;font-size:12px">Взаимодействие с библиотеками</span></summary>
+
+Сначала рассмотрим окончательный вариант CMake (добавим):
+
+```cmake
+cmake_minimum_required(VERSION 3.24)
+project(30_4_1 VERSION 1.0)
+
+set(CMAKE_CXX_STANDARD 17)
+set(BUILD_CPR_TESTS OFF)
+set(CMAKE_USE_OPENSSL OFF)
+# Синхронизирует библиотеки
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+
+include(FetchContent)
+FetchContent_Declare(cpr GIT_REPOSITORY https://github.com/libcpr/cpr.git GIT_TAG 0817715923c9705e68994eb52ef9df3f6845beba)
+FetchContent_MakeAvailable(cpr)
+
+FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz)
+FetchContent_MakeAvailable(json)
+
+
+add_executable(30_4_1 main.cpp)
+
+target_link_libraries(30_4_1 PRIVATE cpr::cpr)
+target_link_libraries(30_4_1 PRIVATE nlohmann_json::nlohmann_json)
+```
+
+В рамках тренировки работы с библиотеками, установим несколько.
+Во-первых, это [nlohmann](https://json.nlohmann.me/integration/cmake/#supporting-both), 
+которая упрощает работу с файлами json.
+
+
 
 </details>
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,146 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using json = nlohmann::json;
+
+enum class Menu { EXIT, GET, POST, PUT, REMOVE, PATCH };
+
+int selectMenuItem(const std::vector<std::string> &list, const std::string &msg = "Choose between options") {
+    std::cout << msg << " (";
+    for (const auto &item : list) { std::cout << item << ((item != list[list.size() - 1]) ? "|" : "):"); }
+
+    while (true) {
+        std::string userInput;
+        std::getline(std::cin >> std::ws, userInput);
+
+        for (int i = 0; i < list.size(); ++i) { if (list[i] == userInput) return i; }
+
+        std::cout << "Error. Try again:";
+    }
+}
+
+void getAsyncRequest(const string &ROOT) {
+    string METHOD = R"(/get)";
+    cpr::Url URL{ROOT + METHOD};
+
+    // Позволяет программе дождаться выполнения запроса
+    bool isRun = true;
+
+    // Используем функцию обратного вызова.
+    auto callback{[&isRun](const cpr::Response &r) {
+        if (r.status_code == 200) {
+            cout << "Get-request completed in " << r.elapsed << endl;
+            cout << "--- Headers ---" << endl;
+            for (const auto &[key, value] : r.header) {
+                cout << std::setw(36) << std::left << key << ": " << value << endl;
+            }
+
+            cout << "--- Text ---" << endl;
+            json jDoc(json::parse(r.text));
+            cout << jDoc.dump(2) << endl;
+
+            // Как пример использования вызова через оператор at
+            try {
+                auto data{jDoc.at("origin").get<string>()};
+                cout << "ORIGIN: " << data << endl;
+            }
+            catch (json::exception &e) { cout << "Trouble" << endl;}
+        }
+        else if (r.status_code == 0) { std::cerr << r.error.message << endl; }
+        else { cout << "Something wrong with status: " << r.status_code << endl; }
+
+        // Ожидание можно завершать
+        isRun = false;
+    }};
+
+    cout << "Loading..." << endl;
+    cpr::GetCallback(callback, URL);
+
+    // Действия выполняются параллельно http запросу
+    cout << "Calculate: " << (5 + 5) << endl;
+
+    while (isRun) {}
+}
+
+void postRequest(const string &ROOT) {
+    string METHOD = R"(/post)";
+    cpr::Url URL{ROOT + METHOD};
+
+    cpr::Header Header{{"content-type", "application/json"}};
+
+    // Создать данные для Body можно просто разобрав строку...
+    // json jPayload(json::parse(R"({"one": 1, "two": 2, "three": 3})"));
+    // а можно сразу создав объект
+    json jPayload(json::object({ {"one", 1}, {"two", 2}, {"three", 3} }));
+    cpr::Body Body(jPayload.dump());
+
+    // Добавление header с контентом равным application/json позволить получить в ответе json
+    cpr::Response r(cpr::Post(URL, Header, Body));
+
+    if (r.status_code == 200) {
+        json jDoc(json::parse(r.text));
+
+        // Можем распечатать все данные
+        // cout << r.text << endl;
+
+        try {
+            // Извлечем то, что передавали в Body(Payload)
+            auto data(jDoc.at("json").get<std::map<string, int>>());
+            for (const auto &i : data) {
+                cout << std::setw(10) << std::left << i.first << ": " << i.second << endl;
+            }
+        }
+        catch (json::exception &e) { cout << "Error " << e.what() << endl; }
+    }
+    else if (r.status_code == 0) { std::cerr << r.error.message << endl; }
+    else { cout << "Something wrong with status: " << r.status_code << endl; }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> menu = { "exit", "get", "post", "put", "delete", "patch" };
+    const string ROOT = R"(https://httpbin.org)";
+
+
+
+
+
+    while(true) {
+        std::cout << "--- Main menu ---" << std::endl;
+        const int command = selectMenuItem(menu);
+
+        if (command == static_cast<int>(Menu::EXIT)) {
+            std::cout << "Menu/exit mode -->" << std::endl;
+            break;
+        }
+
+        if (command == static_cast<int>(Menu::GET)) {
+            std::cout << "Menu/get request -->" << std::endl;
+            getAsyncRequest(ROOT);
+        }
+        else if (command == static_cast<int>(Menu::POST)) {
+            std::cout << "Menu/post request -->" << std::endl;
+            postRequest(ROOT);
+        }
+        else if (command == static_cast<int>(Menu::PATCH)) {
+            std::cout << "Menu/patch request -->" << std::endl;
+        }
+        else if (command == static_cast<int>(Menu::PUT)) {
+            std::cout << "Menu/put request -->" << std::endl;
+        }
+        else if (command == static_cast<int>(Menu::REMOVE)) {
+            std::cout << "Menu/remove request -->" << std::endl;
+        }
+    }
+
+    cout << "THE END!" << endl;
+
     return 0;
 }
