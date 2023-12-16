4749dfe32740032b55132c5b8cb35c0d47a82945
Novak <novaldix@gmail.com>
2023-12-16 08:16:08 +0300

    30_4_1 added delete and other async requests

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@ using std::string;
 using std::vector;
 using json = nlohmann::json;
 
-enum class Menu { EXIT, GET, POST, PUT, REMOVE, PATCH };
+enum class RequestType { GET, POST, PUT, PATCH, REMOVE, EXIT };
+vector<string> requestTypeTitles{"get", "post", "put", "patch", "delete", "exit"};
 
 int selectMenuItem(const std::vector<std::string> &list, const std::string &msg = "Choose between options") {
     std::cout << msg << " (";
@@ -27,7 +28,7 @@ int selectMenuItem(const std::vector<std::string> &list, const std::string &msg 
     }
 }
 
-void getAsyncRequest(const string &ROOT) {
+void asyncGetRequest(const string &ROOT) {
     string METHOD = R"(/get)";
     cpr::Url URL{ROOT + METHOD};
 
@@ -37,7 +38,9 @@ void getAsyncRequest(const string &ROOT) {
     // Используем функцию обратного вызова.
     auto callback{[&isRun](const cpr::Response &r) {
         if (r.status_code == 200) {
-            cout << "Get-request completed in " << r.elapsed << endl;
+            cout << "Request completed in " << r.elapsed << endl;
+
+            // Посмотрим заголовки
             cout << "--- Headers ---" << endl;
             for (const auto &[key, value] : r.header) {
                 cout << std::setw(36) << std::left << key << ": " << value << endl;
@@ -65,78 +68,116 @@ void getAsyncRequest(const string &ROOT) {
     cpr::GetCallback(callback, URL);
 
     // Действия выполняются параллельно http запросу
-    cout << "Calculate: " << (5 + 5) << endl;
+    cout << "Parallel calculate: " << (5 + 5) << endl;
 
     while (isRun) {}
 }
 
-void postRequest(const string &ROOT) {
-    string METHOD = R"(/post)";
+// Объединяет Post, Put, Patch
+void asyncRequest(RequestType type, const string &ROOT, const json &payload) {
+    // Добавление header с контентом равным application/json позволить получить в ответе json
+    cpr::Header Header{{"content-type", "application/json"}};
+    cpr::Body Body(payload.dump());
+
+    string METHOD{R"(/)" + requestTypeTitles[static_cast<int>(type)]};
+    cout << "Method path: " << METHOD << endl;
     cpr::Url URL{ROOT + METHOD};
 
-    cpr::Header Header{{"content-type", "application/json"}};
+    bool isRun = true;
+
+    auto callback{[&isRun](const cpr::Response &r) {
+        if (r.status_code == 200) {
+            cout << "Request completed in " << r.elapsed << endl;
+            // Можем распечатать все данные
+            // cout << r.text << endl;
+            // Разбираем тело ответа. После этого сможем получать доступ к каждому элементу json
+            json jDoc(json::parse(r.text));
+
+            try {
+                // Можем извлечь конкретное поле и, например, итерироваться по переданному ранее payload
+                auto data(jDoc.at("json").get<std::map<string, int>>());
+                for (const auto &i : data) {
+                    cout << std::setw(10) << std::left << i.first << ": " << i.second << endl;
+                }
+            }
+            catch (json::exception &e) { cout << "Error " << e.what() << endl; }
+        }
+        else if (r.status_code == 0) { std::cerr << r.error.message << endl; }
+        else { cout << "Something wrong with status: " << r.status_code << endl; }
+
+        isRun = false;
+    }};
+
+    cout << "Loading..." << endl;
+    if (type == RequestType::POST) {
+        cpr::PostCallback(callback, URL, Header, Body);
+    }
+    else if (type == RequestType::PUT) {
+        cpr::PutCallback(callback, URL, Header, Body);
+    }
+    else if (type == RequestType::PATCH) {
+        cpr::PatchCallback(callback, URL, Header, Body);
+    }
+
+    // Пример асинхронности. Действия выполняются параллельно http запросу
+    cout << "Parallel calculate: " << (5 + 5) << endl;
+
+    while (isRun) {}
+}
+
+void asyncDeleteRequest(const string &ROOT) {
+    string METHOD = R"(/delete)";
+    cpr::Url URL{ROOT + METHOD};
+
+    bool isRun = true;
+
+    auto callback{[&isRun](const cpr::Response &r) {
+        if (r.status_code == 200) {
+            cout << "Request completed in " << r.elapsed << endl;
+            cout << "DELETE OK" << endl;
+            cout << r.text << endl;
+        }
+        else if (r.status_code == 0) { std::cerr << r.error.message << endl; }
+        else { cout << "Something wrong with status: " << r.status_code << endl; }
+
+        isRun = false;
+    }};
+
+    cpr::DeleteCallback(callback, URL);
+
+    while (isRun) {}
+}
+
+int main() {
+    const string ROOT = R"(https://httpbin.org)";
 
     // Создать данные для Body можно просто разобрав строку...
     // json jPayload(json::parse(R"({"one": 1, "two": 2, "three": 3})"));
     // а можно сразу создав объект
     json jPayload(json::object({ {"one", 1}, {"two", 2}, {"three", 3} }));
-    cpr::Body Body(jPayload.dump());
-
-    // Добавление header с контентом равным application/json позволить получить в ответе json
-    cpr::Response r(cpr::Post(URL, Header, Body));
-
-    if (r.status_code == 200) {
-        json jDoc(json::parse(r.text));
-
-        // Можем распечатать все данные
-        // cout << r.text << endl;
-
-        try {
-            // Извлечем то, что передавали в Body(Payload)
-            auto data(jDoc.at("json").get<std::map<string, int>>());
-            for (const auto &i : data) {
-                cout << std::setw(10) << std::left << i.first << ": " << i.second << endl;
-            }
-        }
-        catch (json::exception &e) { cout << "Error " << e.what() << endl; }
-    }
-    else if (r.status_code == 0) { std::cerr << r.error.message << endl; }
-    else { cout << "Something wrong with status: " << r.status_code << endl; }
-}
-
-int main() {
-    vector<string> menu = { "exit", "get", "post", "put", "delete", "patch" };
-    const string ROOT = R"(https://httpbin.org)";
-
-
-
-
 
     while(true) {
         std::cout << "--- Main menu ---" << std::endl;
-        const int command = selectMenuItem(menu);
+        const int command = selectMenuItem(requestTypeTitles);
 
-        if (command == static_cast<int>(Menu::EXIT)) {
+        if (command == static_cast<int>(RequestType::EXIT)) {
             std::cout << "Menu/exit mode -->" << std::endl;
             break;
         }
 
-        if (command == static_cast<int>(Menu::GET)) {
+        if (command == static_cast<int>(RequestType::GET)) {
             std::cout << "Menu/get request -->" << std::endl;
-            getAsyncRequest(ROOT);
+            asyncGetRequest(ROOT);
         }
-        else if (command == static_cast<int>(Menu::POST)) {
-            std::cout << "Menu/post request -->" << std::endl;
-            postRequest(ROOT);
+        else if (command == static_cast<int>(RequestType::POST) ||
+                   command == static_cast<int>(RequestType::PUT) ||
+                   command == static_cast<int>(RequestType::PATCH)) {
+            std::cout << "Menu/" << requestTypeTitles[command] << " request -->" << std::endl;
+            asyncRequest(static_cast<RequestType>(command), ROOT, jPayload);
         }
-        else if (command == static_cast<int>(Menu::PATCH)) {
-            std::cout << "Menu/patch request -->" << std::endl;
-        }
-        else if (command == static_cast<int>(Menu::PUT)) {
-            std::cout << "Menu/put request -->" << std::endl;
-        }
-        else if (command == static_cast<int>(Menu::REMOVE)) {
-            std::cout << "Menu/remove request -->" << std::endl;
+        else if (command == static_cast<int>(RequestType::REMOVE)) {
+            std::cout << "Menu/delete request -->" << std::endl;
+            asyncDeleteRequest(ROOT);
         }
     }
 
